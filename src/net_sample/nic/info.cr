6ed63965a7f3136dee9709a883c2aa781f85b29d
ʕ·ᴥ·ʔAKJ <arcage@denchu.org>
2020-09-30 14:34:32 +0900

    v0.2.5

--- src/net_sample/nic/info.cr
+++ src/net_sample/nic/info.cr
@@ -1,14 +1,16 @@
-struct NetSample::NIC::Info
-  enum Type
-    InAddr
-    In6Addr
-    HWAddr
-  end
+class NetSample::NIC
+  struct Info
+    enum Type
+      InAddr
+      In6Addr
+      HWAddr
+    end
 
-  getter name
-  getter type
-  getter value
+    getter name
+    getter type
+    getter value
 
-  def initialize(@name : String, @type : Type, @value : Bytes)
+    def initialize(@name : String, @type : Type, @value : Bytes)
+    end
   end
 end
